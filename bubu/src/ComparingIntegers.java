f56a4aaec94ea5836f69c67b26937afe9489e5aa
Nesstys <andronic165@gmail.com>
2020-09-11 12:06:25 +0300

    new

---
+++ bubu/src/ComparingIntegers.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class ComparingIntegers {
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+
+        int number1;
+        int number2;
+
+        System.out.print("Introduceti primul numar: ");
+        number1 = input.nextInt();
+
+        System.out.print("Introduceti al doilea numar: ");
+        number2 = input.nextInt();
+
+        if(number1 == number2)
+            System.out.print("Numerele sunt egale");
+        else if (number1 > number2)
+            System.out.printf("%d este mai mare %n",number1);
+        else
+            System.out.printf("%d este mai mare",number2);
+    }
+}
